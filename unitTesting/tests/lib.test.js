d4b55a23877958fe47ba60d8dc8ad128df1d01bc
erick.augustor <erick.augustoramos@gmail.com>
2018-07-22 20:59:47 -0300

    Unit Testing

--- unitTesting/tests/lib.test.js
+++ unitTesting/tests/lib.test.js
@@ -1,19 +1,51 @@
 const lib = require('../lib');
 
-test('absolute - should return a positive number if input is positive', () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-  // throw new Error('Something failed');
+describe('absolute', () => {
+  it('should return a positive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+    // throw new Error('Something failed');
+  });
+
+  it('should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it('should return 0 if input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+}); 
+
+describe('greet', () => {
+  it('should return the greeting message', () => {
+    const result = lib.greet('Erick');
+    expect(result).toMatch(/Erick/);
+    expect(result).toContain('Erick');
+  })
 });
 
-test('absolute - should return a positive number if input is negative', () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-  // throw new Error('Something failed');
-});
+describe('getCurrencies', () => {
+  it('should return supprted currencies', () => {
+    const result = lib.getCurrencies();
 
-test('absolute - should return 0 if input is 0', () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
-  // throw new Error('Something failed');
+    // Too general
+    expect(result).toBeDefined();
+    expect(result).not.toBeNull();
+
+    // Too specific
+    expect(result[0]).toBe('USD');
+    expect(result[1]).toBe('AUD');
+    expect(result[2]).toBe('EUR');
+    expect(result.length).toBe(3);
+
+    // Proper way
+    expect(result).toContain('USD');
+    expect(result).toContain('AUD');
+    expect(result).toContain('EUR');
+
+    // Ideal way
+    expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']));
+  });
 });
