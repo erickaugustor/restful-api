68f697fae30ff970606b2b8275902e222ca6d875
erick.augustor <erick.augustoramos@gmail.com>
2018-07-26 20:43:02 -0300

    Authentication and Authorization

--- restfulProjectVidly/models/user.js
+++ restfulProjectVidly/models/user.js
@@ -1,7 +1,9 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const genreSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -23,7 +25,12 @@ const genreSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', genreSchema);
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+}
+
+const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
   const schema = {
--- restfulProjectVidly/routes/auth.js
+++ restfulProjectVidly/routes/auth.js
@@ -20,12 +20,19 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
+
+  const token = user.generateAuthToken();
+
   // JSON Web Tokens
   // config.get() === environment variables
-  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey') );
+  // const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey') );
+  
   res.send(token);
-
 });
+
+// Information Expert Principle
+
+
 
 function validate(req) {
   const schema = {
